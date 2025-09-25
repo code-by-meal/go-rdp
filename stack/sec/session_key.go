aa0395ca4829c8e6b432b20ddc9d2c602decfa8e
mdmytro <mdmytro566@gmail.com>
2025-09-25 22:11:38 +0300

    action: finish session keys

--- stack/sec/session_key.go
+++ stack/sec/session_key.go
@@ -10,21 +10,32 @@ import (
 )
 
 var (
-	A   = []byte("A")
-	BB  = []byte("BB")
-	CCC = []byte("CCC")
-	X   = []byte("X")
-	YY  = []byte("YY")
-	ZZZ = []byte("ZZZ")
+	A    = []byte("A")
+	BB   = []byte("BB")
+	CCC  = []byte("CCC")
+	X    = []byte("X")
+	YY   = []byte("YY")
+	ZZZ  = []byte("ZZZ")
+	Salt = []byte{0xD1, 0x26, 0x9E}
 )
 
 type SessionKeys struct {
-	EncryptMethod      serverdata.EncryptMethod
-	PreMasterSecretKey []byte // 48 bytes
-	MasterSecretKey    []byte // 48 bytes
-	SessionKeyBlob     []byte // 48 bytes
-	ClientRandom       []byte // 32 bytes
-	ServerRandom       []byte // 32 bytes
+	EncryptMethod           serverdata.EncryptMethod
+	PreMasterSecretKey      []byte // 48 bytes
+	MasterSecretKey         []byte // 48 bytes
+	SessionKeyBlob          []byte // 48 bytes
+	ClientRandom            []byte // 32 bytes
+	ServerRandom            []byte // 32 bytes
+	SignKey                 []byte // 16 bytes
+	EncryptKey              []byte // 16 bytes
+	DecryptKey              []byte // 16 bytes
+	UpdateEncryptKey        []byte // 16 bytes
+	UpdateDecryptKey        []byte // 16 bytes
+	RC4KeyLen               int
+	DecryptUseCount         int
+	DecryptChecksumUseCount int
+	EncryptUseCount         int
+	EncryptChecksumUseCount int
 }
 
 func NewSessionKey(encryptMethod serverdata.EncryptMethod, clientRandom, serverRandom []byte) (*SessionKeys, error) {
@@ -33,12 +44,16 @@ func NewSessionKey(encryptMethod serverdata.EncryptMethod, clientRandom, serverR
 	}
 
 	return &SessionKeys{
-		EncryptMethod:      encryptMethod,
-		PreMasterSecretKey: []byte{},
-		MasterSecretKey:    []byte{},
-		SessionKeyBlob:     []byte{},
-		ClientRandom:       clientRandom,
-		ServerRandom:       serverRandom,
+		EncryptMethod:           encryptMethod,
+		PreMasterSecretKey:      []byte{},
+		MasterSecretKey:         []byte{},
+		SessionKeyBlob:          []byte{},
+		ClientRandom:            clientRandom,
+		ServerRandom:            serverRandom,
+		DecryptChecksumUseCount: 0,
+		DecryptUseCount:         0,
+		EncryptChecksumUseCount: 0,
+		EncryptUseCount:         0,
 	}, nil
 }
 
@@ -54,7 +69,6 @@ func (s *SessionKeys) Calc() error {
 
 func (s *SessionKeys) EstablishKeys() error {
 	prefix := "sess keys: establish keys: %w"
-	_ = prefix
 	preMasterSecretKey := []byte{}
 	preMasterSecretKey = append(preMasterSecretKey, s.ClientRandom[:24]...)
 	preMasterSecretKey = append(preMasterSecretKey, s.ServerRandom[:24]...)
@@ -94,7 +108,65 @@ func (s *SessionKeys) EstablishKeys() error {
 	log.Dbg("<s>Out X:</>", outX)
 	s.SessionKeyBlob = outX
 
+	// Calculating decrypt session key
+	decryptKey, err := s.AllowsFIPS(s.SessionKeyBlob[16:32])
+
+	if err != nil {
+		return fmt.Errorf(prefix, err)
+	}
+
+	s.DecryptKey = decryptKey
+	log.Dbg("<s>Decrypt key:</>", decryptKey)
+
+	// Calculating encrypt session key
+	encryptKey, err := s.AllowsFIPS(s.SessionKeyBlob[32:48])
+
+	if err != nil {
+		return fmt.Errorf(prefix, err)
+	}
+
+	s.EncryptKey = encryptKey
+	log.Dbg("<s>Encrypt Key:</>", encryptKey)
+
+	// Weaking sessin keys
+	switch s.EncryptMethod { // nolint
+	case serverdata.Four0BIT:
+		Copy(&s.SignKey, &Salt, 3)
+		Copy(&s.DecryptKey, &Salt, 3)
+		Copy(&s.EncryptKey, &Salt, 3)
+
+		s.RC4KeyLen = 8
+
+	case serverdata.Five6BIT:
+		Copy(&s.SignKey, &Salt, 1)
+		Copy(&s.DecryptKey, &Salt, 1)
+		Copy(&s.EncryptKey, &Salt, 1)
+
+		s.RC4KeyLen = 8
+
+	case serverdata.One28BIT:
+		s.RC4KeyLen = 16
+	}
+
 	return nil
+}
+
+func Copy(dst, src *[]byte, length int) {
+	for i := range length {
+		(*dst)[i] = (*src)[i]
+	}
+}
+
+func (s *SessionKeys) AllowsFIPS(input []byte) ([]byte, error) {
+	m5 := md5.New()
+
+	for _, arr := range [][]byte{input, s.ClientRandom, s.ServerRandom} {
+		if _, err := m5.Write(arr); err != nil {
+			return []byte{}, fmt.Errorf("sess keys: allow fips: %w", err)
+		}
+	}
+
+	return m5.Sum(nil), nil
 }
 
 func (s *SessionKeys) SaltedHash(input, salt1, salt2, salt3 []byte) ([]byte, error) {
